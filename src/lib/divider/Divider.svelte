439ac20f21780011c416f819c8005f26388fc79c
zzh <18819423808@163.com>
2023-11-26 15:31:29 +0800

    feat: divider

--- src/lib/divider/Divider.svelte
+++ src/lib/divider/Divider.svelte
@@ -1,1 +1,15 @@
-<div>divider</div>
+<script>
+  import { twMerge } from 'tailwind-merge';
+  let dividerClasses;
+  $: dividerClasses = twMerge(
+    'w-full min-w-full flex clear-both my-4 h-0 border-t border-gray-400'
+  )
+</script>
+
+{#if $$slots}
+<div class={dividerClasses}>
+  <slot />
+</div>
+{:else}
+<div class={dividerClasses}></div>
+{/if}
