8bd471866545a6778136dde781457fbd51abb9eb
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-11-27 10:07:19 +0800

    feat: rename divider to seperator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,6 @@
 		Collapse,
 		ColorPicker,
 		DatePicker,
-		Divider,
 		Drawer,
 		Dropdown,
 		Form,
@@ -40,6 +39,7 @@
 		Radio,
 		Row,
 		Select,
+		Seperator,
 		Skeleton,
 		Slider,
 		Space,
@@ -57,72 +57,70 @@
 	} from '$lib';
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Affix></Affix>
-<Alert></Alert>
-<Anchor></Anchor>
-<AutoComplete></AutoComplete>
-<Avatar></Avatar>
-<BackTop></BackTop>
-<Badge></Badge>
-<Breadcrumb></Breadcrumb>
-<hr/>
-<Button>default</Button>
-<Button type="primary">primary</Button>
-<Button type="dashed">dashed</Button>
-<Button type="text">text</Button>
-<Button type="link">link</Button>
-<hr/>
-<ButtonGroup>
+<div class="flex flex-col">
+	<Affix></Affix>
+	<Alert></Alert>
+	<Anchor></Anchor>
+	<AutoComplete></AutoComplete>
+	<Avatar></Avatar>
+	<BackTop></BackTop>
+	<Badge></Badge>
+	<Breadcrumb></Breadcrumb>
+	<hr />
 	<Button>default</Button>
 	<Button type="primary">primary</Button>
 	<Button type="dashed">dashed</Button>
-</ButtonGroup>
-<hr />
-<Calendar></Calendar>
-<Card></Card>
-<Carousel></Carousel>
-<Cascader></Cascader>
-<Checkbox></Checkbox>
-<Col></Col>
-<Collapse></Collapse>
-<ColorPicker></ColorPicker>
-<DatePicker></DatePicker>
-<Divider></Divider>
-<Drawer></Drawer>
-<Dropdown></Dropdown>
-<Form></Form>
-<Grid></Grid>
-<InfiniteScroll></InfiniteScroll>
-<Input></Input>
-<InputNumber></InputNumber>
-<List></List>
-<Loading></Loading>
-<Menu></Menu>
-<Message></Message>
-<Modal></Modal>
-<Notification></Notification>
-<Pagination></Pagination>
-<Popconfirm></Popconfirm>
-<Popover></Popover>
-<Progress progress={45}></Progress>
-<Radio></Radio>
-<Row></Row>
-<Select></Select>
-<Skeleton></Skeleton>
-<Slider></Slider>
-<Space></Space>
-<Steps></Steps>
-<Switch></Switch>
-<Table></Table>
-<Tabs></Tabs>
-<Tag></Tag>
-<TimePicker></TimePicker>
-<Timeline></Timeline>
-<Tooltip></Tooltip>
-<Transfer></Transfer>
-<Tree></Tree>
-<Upload></Upload>
+	<Button type="text">text</Button>
+	<Button type="link">link</Button>
+	<hr />
+	<ButtonGroup>
+		<Button>default</Button>
+		<Button type="primary">primary</Button>
+		<Button type="dashed">dashed</Button>
+	</ButtonGroup>
+	<hr />
+	<Calendar></Calendar>
+	<Card></Card>
+	<Carousel></Carousel>
+	<Cascader></Cascader>
+	<Checkbox></Checkbox>
+	<Col></Col>
+	<Collapse></Collapse>
+	<ColorPicker></ColorPicker>
+	<DatePicker></DatePicker>
+	<Drawer></Drawer>
+	<Dropdown></Dropdown>
+	<Form></Form>
+	<Grid></Grid>
+	<InfiniteScroll></InfiniteScroll>
+	<Input></Input>
+	<InputNumber></InputNumber>
+	<List></List>
+	<Loading></Loading>
+	<Menu></Menu>
+	<Message></Message>
+	<Modal></Modal>
+	<Notification></Notification>
+	<Pagination></Pagination>
+	<Popconfirm></Popconfirm>
+	<Popover></Popover>
+	<Progress progress={45}></Progress>
+	<Radio></Radio>
+	<Row></Row>
+	<Select></Select>
+	<Seperator />
+	<Skeleton></Skeleton>
+	<Slider></Slider>
+	<Space></Space>
+	<Steps></Steps>
+	<Switch></Switch>
+	<Table></Table>
+	<Tabs></Tabs>
+	<Tag></Tag>
+	<TimePicker></TimePicker>
+	<Timeline></Timeline>
+	<Tooltip></Tooltip>
+	<Transfer></Transfer>
+	<Tree></Tree>
+	<Upload></Upload>
+</div>
