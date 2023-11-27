812aecc3d74e335372c80b8a0aa64429eb996cbc
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-11-27 18:18:01 +0800

    wip: space

--- src/lib/space/Space.svelte
+++ src/lib/space/Space.svelte
@@ -1,1 +1,27 @@
-<div>space</div>
+<script lang="ts">
+  import { twMerge } from 'tailwind-merge';
+
+  export let direction: 'horizontal' | 'vertical' = 'horizontal';
+  let spaceClasses = 'mr-2 last:mr-0';
+  
+  function space(node: HTMLElement) {
+    console.log('zzh node', [node]);
+    node.childNodes = node.childNodes.forEach((element) => {
+      const wrapItem = document.createElement('div');
+      wrapItem.className = 'z-space-item';
+      wrapItem.appendChild(element);
+    });
+    const destroy = () => {
+      console.log('zzh node', node);
+    };
+    return { destroy };
+  }
+
+  let spaceWrapperClasses = twMerge(
+    direction === 'horizontal' ? 'flex-row' : 'flex-col'
+  );
+</script>
+
+<div use:space class={spaceWrapperClasses}>
+  <slot />
+</div>
