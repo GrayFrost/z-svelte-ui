c2973cef6a92a48a23a7db27a536b2888de9c8d6
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-12-06 18:08:02 +0800

    feat: docs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,19 @@
   import "../app.pcss";
 </script>
 
-<header class="sticky"></header>
+<header class="sticky top-0 z-40">
+  <ul>
+    <li>
+      <a href="/docs/pages/docs">Docs</a>
+    </li>
+    <li>
+      <a href="/docs/pages/quickstart">Quickstart</a> 
+    </li>
+    <li>
+      <a href="/docs/components/affix">Components</a>
+    </li>
+  </ul>
+</header>
 
 <div class="w-full">
   <slot></slot>
